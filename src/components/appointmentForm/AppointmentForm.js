60669a7d9c3505f7b53d499a02548037e7b05ad9
AaronSBF <aaronkelly883@gmail.com>
2022-09-21 14:01:15 +0000

    added appointment form

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
-export const AppointmentsPage = ({
+export const AppointmentForm = ({
   contacts,
   title,
   setTitle,
@@ -12,19 +13,38 @@ export const AppointmentsPage = ({
   setTime,
   handleSubmit,
 }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const getTodayString = () => {
+    const [month, day, year] = new Date()
+      .toLocaleDateString("en-US")
+      .split("/");
+    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
 
   return (
-    <div>
-      <section>
-        <h2>Add Appointment</h2>
-      </section>
-      <hr />
-      <section>
-        <h2>Appointments</h2>
-      </section>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        placeholder="Title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="date"
+        value={date}
+        onChange={({ target }) => setDate(target.value)}
+        min={getTodayString()}
+      />
+      <input
+        type="time"
+        value={time}
+        onChange={({ target }) => setTime(target.value)}
+      />
+      <ContactPicker
+        contacts={contacts}
+        value={contact}
+        onChange={({ target }) => setContact(target.value)}
+      />
+      <input type="submit" className="submit-btn" />
+    </form>
   );
 };
--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = (props) => {
+  const appointments = props.appointments;
+  const addAppointment = props.addAppointment;
+  const contacts = props.contacts;
+
+  const [title, setTitle] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
-    Add contact info and clear data  
+    Added contact info and clear data  
     */
-   
+    addAppointment(title, contact, date, time);
+    setTitle("");
+    setContact("");
+    setDate("");
+    setTime("");
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          title={title}
+          contact={contact}
+          date={date}
+          time={time}
+          setTitle={setTitle}
+          setContact={setContact}
+          setDate={setDate}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
+          contacts={contacts}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList data={appointments} />
       </section>
     </div>
   );
